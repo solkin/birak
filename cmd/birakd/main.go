solkin/sebastian

Add multipart upload staging store and janitor

cmd/sebastiand/main.go
@@ -40,7 +40,8 @@ func main() {
 	}
 
 	// Clear scratch files left by a previous crash before serving any requests.
-	gateway.SweepTempFiles(cfg.RootDir, logger)
+	// No upload can be in flight yet, so age is irrelevant here.
+	gateway.SweepTempFiles(cfg.RootDir, 0, logger)
 
 	logger.Info("sebastian starting",
 		"root_dir", cfg.RootDir,

internal/gateway/gateway.go
@@ -11,24 +11,49 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
+// ReservedDirName is a top-level directory under rootDir that sebastian reserves
+// for its own state (multipart upload staging). It is not a bucket and is hidden
+// from every gateway: SafePath refuses to resolve into it and directory listings
+// skip it, so clients can neither see nor corrupt in-progress uploads.
+const ReservedDirName = ".sebastian"
+
+// IsReserved reports whether name is the reserved state directory. Gateways call
+// it when filtering the entries of rootDir itself.
+func IsReserved(name string) bool {
+	return name == ReservedDirName
+}
+
 // SweepTempFiles removes stale atomic-write scratch files (".seb-tmp-*" and
-// ".seb-bak-*") left under rootDir by a previous process that died between
-// creating a temp file and renaming it into place. It is safe to call once at
-// startup, when no uploads are in flight.
-func SweepTempFiles(rootDir string, logger *slog.Logger) {
+// ".seb-bak-*") left under rootDir by a process that died between creating a temp
+// file and renaming it into place.
+//
+// Only files last modified more than maxAge ago are removed, so the sweep can run
+// periodically alongside live traffic without destroying an upload that is still
+// streaming. Pass maxAge <= 0 to remove every scratch file regardless of age —
+// correct at startup, when no upload can be in flight.
+func SweepTempFiles(rootDir string, maxAge time.Duration, logger *slog.Logger) {
+	cutoff := time.Now().Add(-maxAge)
 	filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil || d.IsDir() {
 			return nil
 		}
 		name := d.Name()
-		if strings.HasPrefix(name, ".seb-tmp-") || strings.HasPrefix(name, ".seb-bak-") {
-			if rmErr := os.Remove(path); rmErr != nil {
-				logger.Warn("failed to remove stale temp file", "path", path, "error", rmErr)
-			} else {
-				logger.Info("removed stale temp file", "path", path)
+		if !strings.HasPrefix(name, ".seb-tmp-") && !strings.HasPrefix(name, ".seb-bak-") {
+			return nil
+		}
+		if maxAge > 0 {
+			info, statErr := d.Info()
+			if statErr != nil || info.ModTime().After(cutoff) {
+				return nil
 			}
+		}
+		if rmErr := os.Remove(path); rmErr != nil {
+			logger.Warn("failed to remove stale temp file", "path", path, "error", rmErr)
+		} else {
+			logger.Info("removed stale temp file", "path", path)
 		}
 		return nil
 	})
@@ -61,6 +86,12 @@ func SafePath(rootDir, reqPath string) (relPath string, fullPath string, err err
 
 	if cleaned == "" {
 		return "", rootDir, nil
+	}
+
+	// The reserved state directory holds sebastian's own bookkeeping (staged
+	// multipart parts); no protocol may read, write, or delete inside it.
+	if cleaned == ReservedDirName || strings.HasPrefix(cleaned, ReservedDirName+"/") {
+		return "", "", fmt.Errorf("reserved path")
 	}
 
 	full := filepath.Join(rootDir, filepath.FromSlash(cleaned))

internal/multipart/janitor.go
@@ -1,0 +1,62 @@
+package multipart
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/solkin/sebastian/internal/gateway"
+)
+
+// Janitor periodically reclaims space that no client will ever ask for again:
+// multipart uploads that were initiated but never completed or aborted, and
+// atomic-write scratch files orphaned by a process that died mid-write.
+//
+// It runs one sweep immediately at start — a server that was down past the TTL
+// should not wait a full interval before cleaning up — and then on a ticker.
+type Janitor struct {
+	store  *Store
+	logger *slog.Logger
+}
+
+// NewJanitor returns a janitor for the store.
+func NewJanitor(store *Store, logger *slog.Logger) *Janitor {
+	return &Janitor{
+		store:  store,
+		logger: logger.With("component", "multipart-janitor"),
+	}
+}
+
+// Run sweeps until ctx is cancelled.
+func (j *Janitor) Run(ctx context.Context) {
+	interval := j.store.limits.CleanupInterval
+	j.logger.Info("multipart janitor started",
+		"interval", interval.String(),
+		"upload_ttl", j.store.limits.UploadTTL.String(),
+		"temp_file_max_age", j.store.limits.TempFileMaxAge.String(),
+	)
+
+	j.sweep()
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			j.logger.Info("multipart janitor stopped")
+			return
+		case <-ticker.C:
+			j.sweep()
+		}
+	}
+}
+
+// sweep runs one cleanup pass.
+func (j *Janitor) sweep() {
+	if removed := j.store.Cleanup(time.Now()); removed > 0 {
+		j.logger.Info("expired multipart uploads removed", "count", removed)
+	}
+	// Age-bounded so a scratch file belonging to an upload that is still streaming
+	// is never pulled out from under it.
+	gateway.SweepTempFiles(j.store.rootDir, j.store.limits.TempFileMaxAge, j.logger)
+}

solkin/birak

cmd/birakd/main.go
@@ -64,7 +64,8 @@ func run(configPath string) error {
 	}
 
 	// Clear scratch files left by a previous crash before serving any requests.
-	gateway.SweepTempFiles(cfg.SyncDir, logger)
+	// No upload can be in flight yet, so age is irrelevant here.
+	gateway.SweepTempFiles(cfg.SyncDir, 0, logger)
 
 	// Warn about enabled gateways running without credentials.
 	for _, warning := range config.SecurityWarnings(cfg) {

internal/gateway/gateway.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/birak/birak/internal/watcher"
 )
@@ -27,22 +28,46 @@ type Gateway interface {
 	Name() string
 }
 
+// ReservedDirName is a top-level directory under rootDir that Birak reserves
+// for its own state (multipart upload staging). It is not a bucket and is hidden
+// from every gateway: SafePath refuses to resolve into it and directory listings
+// skip it, so clients can neither see nor corrupt in-progress uploads.
+const ReservedDirName = ".birak"
+
+// IsReserved reports whether name is the reserved state directory. Gateways call
+// it when filtering the entries of rootDir itself.
+func IsReserved(name string) bool {
+	return name == ReservedDirName
+}
+
 // SweepTempFiles removes stale atomic-write scratch files (".birak-tmp-*" and
-// ".birak-bak-*") left under rootDir by a previous process that died between
-// creating a temp file and renaming it into place. It is safe to call once at
-// startup, when no uploads are in flight.
-func SweepTempFiles(rootDir string, logger *slog.Logger) {
+// ".birak-bak-*") left under rootDir by a process that died between creating a temp
+// file and renaming it into place.
+//
+// Only files last modified more than maxAge ago are removed, so the sweep can run
+// periodically alongside live traffic without destroying an upload that is still
+// streaming. Pass maxAge <= 0 to remove every scratch file regardless of age —
+// correct at startup, when no upload can be in flight.
+func SweepTempFiles(rootDir string, maxAge time.Duration, logger *slog.Logger) {
+	cutoff := time.Now().Add(-maxAge)
 	filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil || d.IsDir() {
 			return nil
 		}
 		name := d.Name()
-		if strings.HasPrefix(name, ".birak-tmp-") || strings.HasPrefix(name, ".birak-bak-") {
-			if rmErr := os.Remove(path); rmErr != nil {
-				logger.Warn("failed to remove stale temp file", "path", path, "error", rmErr)
-			} else {
-				logger.Info("removed stale temp file", "path", path)
+		if !strings.HasPrefix(name, ".birak-tmp-") && !strings.HasPrefix(name, ".birak-bak-") {
+			return nil
+		}
+		if maxAge > 0 {
+			info, statErr := d.Info()
+			if statErr != nil || info.ModTime().After(cutoff) {
+				return nil
 			}
+		}
+		if rmErr := os.Remove(path); rmErr != nil {
+			logger.Warn("failed to remove stale temp file", "path", path, "error", rmErr)
+		} else {
+			logger.Info("removed stale temp file", "path", path)
 		}
 		return nil
 	})
@@ -68,6 +93,12 @@ func SafePath(rootDir, reqPath string, ignorePatterns []string) (relPath string,
 
 	if cleaned == "" {
 		return "", rootDir, nil
+	}
+
+	// The reserved state directory holds Birak's own bookkeeping (staged
+	// multipart parts); no protocol may read, write, or delete inside it.
+	if cleaned == ReservedDirName || strings.HasPrefix(cleaned, ReservedDirName+"/") {
+		return "", "", fmt.Errorf("reserved path")
 	}
 
 	full := filepath.Join(rootDir, filepath.FromSlash(cleaned))

internal/multipart/janitor.go
@@ -1,0 +1,62 @@
+package multipart
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/birak/birak/internal/gateway"
+)
+
+// Janitor periodically reclaims space that no client will ever ask for again:
+// multipart uploads that were initiated but never completed or aborted, and
+// atomic-write scratch files orphaned by a process that died mid-write.
+//
+// It runs one sweep immediately at start — a server that was down past the TTL
+// should not wait a full interval before cleaning up — and then on a ticker.
+type Janitor struct {
+	store  *Store
+	logger *slog.Logger
+}
+
+// NewJanitor returns a janitor for the store.
+func NewJanitor(store *Store, logger *slog.Logger) *Janitor {
+	return &Janitor{
+		store:  store,
+		logger: logger.With("component", "multipart-janitor"),
+	}
+}
+
+// Run sweeps until ctx is cancelled.
+func (j *Janitor) Run(ctx context.Context) {
+	interval := j.store.limits.CleanupInterval
+	j.logger.Info("multipart janitor started",
+		"interval", interval.String(),
+		"upload_ttl", j.store.limits.UploadTTL.String(),
+		"temp_file_max_age", j.store.limits.TempFileMaxAge.String(),
+	)
+
+	j.sweep()
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			j.logger.Info("multipart janitor stopped")
+			return
+		case <-ticker.C:
+			j.sweep()
+		}
+	}
+}
+
+// sweep runs one cleanup pass.
+func (j *Janitor) sweep() {
+	if removed := j.store.Cleanup(time.Now()); removed > 0 {
+		j.logger.Info("expired multipart uploads removed", "count", removed)
+	}
+	// Age-bounded so a scratch file belonging to an upload that is still streaming
+	// is never pulled out from under it.
+	gateway.SweepTempFiles(j.store.rootDir, j.store.limits.TempFileMaxAge, j.logger)
+}