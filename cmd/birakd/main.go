solkin/sebastian

Add S3 multipart upload API and hide reserved state dir

cmd/sebastiand/main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/solkin/sebastian/internal/gateway/s3"
 	"github.com/solkin/sebastian/internal/gateway/sftp"
 	"github.com/solkin/sebastian/internal/gateway/webdav"
+	"github.com/solkin/sebastian/internal/multipart"
 )
 
 func main() {
@@ -61,13 +62,35 @@ func main() {
 	var gateways []gateway.Gateway
 	errCh := make(chan error, 4)
 
+	// The multipart staging store is only reachable through the S3 API, but its
+	// janitor also sweeps scratch files written by the other gateways, so it runs
+	// whenever S3 is enabled.
+	var mpStore *multipart.Store
 	if cfg.Gateways.S3.Enabled {
+		store, err := multipart.New(cfg.RootDir, multipart.Limits{
+			MinPartBytes:       cfg.Multipart.MinPartBytes,
+			MaxPartBytes:       cfg.Multipart.MaxPartBytes,
+			MaxParts:           cfg.Multipart.MaxParts,
+			MaxObjectBytes:     cfg.MaxUploadBytes,
+			MaxActiveUploads:   cfg.Multipart.MaxActiveUploads,
+			MaxConcurrentParts: cfg.Multipart.MaxConcurrentPartUploads,
+			UploadTTL:          cfg.Multipart.UploadTTL,
+			CleanupInterval:    cfg.Multipart.CleanupInterval,
+			TempFileMaxAge:     cfg.Multipart.TempFileMaxAge,
+		}, logger)
+		if err != nil {
+			logger.Error("failed to initialize multipart store", "error", err)
+			os.Exit(1)
+		}
+		mpStore = store
+
 		gw := s3.New(cfg.RootDir, s3.Config{
 			ListenAddr:     cfg.Gateways.S3.ListenAddr,
 			AccessKey:      cfg.Gateways.S3.AccessKey,
 			SecretKey:      cfg.Gateways.S3.SecretKey,
 			Domain:         cfg.Gateways.S3.Domain,
 			MaxUploadBytes: cfg.MaxUploadBytes,
+			Multipart:      mpStore,
 		}, logger)
 		gateways = append(gateways, gw)
 	}
@@ -105,6 +128,10 @@ func main() {
 			os.Exit(1)
 		}
 		gateways = append(gateways, gw)
+	}
+
+	if mpStore != nil {
+		go multipart.NewJanitor(mpStore, logger).Run(ctx)
 	}
 
 	for _, gw := range gateways {

internal/config/config.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"gopkg.in/yaml.v3"
 )
@@ -16,8 +17,34 @@ type Config struct {
 	RootDir string `yaml:"root_dir"`
 	// MaxUploadBytes caps the size of a single uploaded object/file across the S3,
 	// WebDAV, and HTTP UI gateways. 0 means unlimited.
-	MaxUploadBytes int64    `yaml:"max_upload_bytes"`
-	Gateways       Gateways `yaml:"gateways"`
+	MaxUploadBytes int64           `yaml:"max_upload_bytes"`
+	Multipart      MultipartConfig `yaml:"multipart"`
+	Gateways       Gateways        `yaml:"gateways"`
+}
+
+// MultipartConfig holds limits and retention settings for S3 multipart uploads.
+// Sizes are in bytes; durations use Go duration syntax ("24h", "30m").
+type MultipartConfig struct {
+	// MinPartBytes is the minimum size of every part but the last (S3: 5 MiB).
+	MinPartBytes int64 `yaml:"min_part_bytes"`
+	// MaxPartBytes is the maximum size of a single part (S3: 5 GiB).
+	MaxPartBytes int64 `yaml:"max_part_bytes"`
+	// MaxParts is the highest accepted part number (S3: 10000).
+	MaxParts int `yaml:"max_parts"`
+	// MaxActiveUploads caps simultaneously staged uploads; 0 means unlimited.
+	MaxActiveUploads int `yaml:"max_active_uploads"`
+	// MaxConcurrentPartUploads caps in-flight part uploads across all uploads;
+	// 0 means unlimited. Requests over the cap are rejected with SlowDown, which
+	// every S3 SDK retries with backoff.
+	MaxConcurrentPartUploads int `yaml:"max_concurrent_part_uploads"`
+	// UploadTTL is how long an untouched incomplete upload is kept before the
+	// janitor discards it.
+	UploadTTL time.Duration `yaml:"upload_ttl"`
+	// CleanupInterval is how often the janitor sweeps.
+	CleanupInterval time.Duration `yaml:"cleanup_interval"`
+	// TempFileMaxAge is how old an orphaned atomic-write scratch file must be
+	// before a running server removes it.
+	TempFileMaxAge time.Duration `yaml:"temp_file_max_age"`
 }
 
 // Gateways groups all gateway configurations.
@@ -103,6 +130,10 @@ func applyDefaults(cfg *Config) {
 	if cfg.Gateways.SFTP.ListenAddr == "" {
 		cfg.Gateways.SFTP.ListenAddr = ":9500"
 	}
+
+	// Unset multipart fields stay zero and are resolved to the S3-compatible
+	// defaults by the multipart store, so there is a single source of truth for
+	// them.
 }
 
 func applyEnv(cfg *Config) {
@@ -146,6 +177,44 @@ func applyEnv(cfg *Config) {
 			cfg.MaxUploadBytes = n
 		}
 	}
+
+	envInt64Map := map[string]*int64{
+		"SEBASTIAN_MULTIPART_MIN_PART_BYTES": &cfg.Multipart.MinPartBytes,
+		"SEBASTIAN_MULTIPART_MAX_PART_BYTES": &cfg.Multipart.MaxPartBytes,
+	}
+	for env, ptr := range envInt64Map {
+		if v := os.Getenv(env); v != "" {
+			if n, err := strconv.ParseInt(strings.TrimSpace(v), 10, 64); err == nil && n >= 0 {
+				*ptr = n
+			}
+		}
+	}
+
+	envIntMap := map[string]*int{
+		"SEBASTIAN_MULTIPART_MAX_PARTS":                   &cfg.Multipart.MaxParts,
+		"SEBASTIAN_MULTIPART_MAX_ACTIVE_UPLOADS":          &cfg.Multipart.MaxActiveUploads,
+		"SEBASTIAN_MULTIPART_MAX_CONCURRENT_PART_UPLOADS": &cfg.Multipart.MaxConcurrentPartUploads,
+	}
+	for env, ptr := range envIntMap {
+		if v := os.Getenv(env); v != "" {
+			if n, err := strconv.Atoi(strings.TrimSpace(v)); err == nil && n >= 0 {
+				*ptr = n
+			}
+		}
+	}
+
+	envDurationMap := map[string]*time.Duration{
+		"SEBASTIAN_MULTIPART_UPLOAD_TTL":        &cfg.Multipart.UploadTTL,
+		"SEBASTIAN_MULTIPART_CLEANUP_INTERVAL":  &cfg.Multipart.CleanupInterval,
+		"SEBASTIAN_MULTIPART_TEMP_FILE_MAX_AGE": &cfg.Multipart.TempFileMaxAge,
+	}
+	for env, ptr := range envDurationMap {
+		if v := os.Getenv(env); v != "" {
+			if d, err := time.ParseDuration(strings.TrimSpace(v)); err == nil && d > 0 {
+				*ptr = d
+			}
+		}
+	}
 }
 
 func parseBool(s string) bool {
@@ -167,6 +236,25 @@ func validate(cfg *Config) error {
 
 	if cfg.Gateways.SFTP.Enabled && cfg.Gateways.SFTP.HostKeyPath == "" {
 		return fmt.Errorf("sftp.host_key_path is required when SFTP gateway is enabled")
+	}
+
+	m := cfg.Multipart
+	if m.MinPartBytes < 0 || m.MaxPartBytes < 0 {
+		return fmt.Errorf("multipart part size limits must not be negative")
+	}
+	// A minimum above the maximum would reject every multi-part upload with a
+	// contradictory pair of errors, so refuse it at startup instead.
+	if m.MinPartBytes > 0 && m.MaxPartBytes > 0 && m.MinPartBytes > m.MaxPartBytes {
+		return fmt.Errorf("multipart.min_part_bytes must not exceed multipart.max_part_bytes")
+	}
+	if m.MaxParts < 0 {
+		return fmt.Errorf("multipart.max_parts must not be negative")
+	}
+	if m.MaxActiveUploads < 0 || m.MaxConcurrentPartUploads < 0 {
+		return fmt.Errorf("multipart upload count limits must not be negative")
+	}
+	if m.UploadTTL < 0 || m.CleanupInterval < 0 || m.TempFileMaxAge < 0 {
+		return fmt.Errorf("multipart durations must not be negative")
 	}
 
 	return nil

internal/gateway/s3/s3.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/solkin/sebastian/internal/gateway"
+	"github.com/solkin/sebastian/internal/multipart"
 )
 
 // Config holds S3 gateway configuration.
@@ -21,22 +22,28 @@ type Config struct {
 	SecretKey      string `yaml:"secret_key"`
 	Domain         string `yaml:"domain"`
 	MaxUploadBytes int64  // max object size for PutObject; 0 = unlimited
+	// Multipart is the staging store backing the multipart upload API. When nil,
+	// multipart operations answer NotImplemented and the rest of the gateway is
+	// unaffected.
+	Multipart *multipart.Store
 }
 
 // Gateway implements the S3-compatible API.
 type Gateway struct {
-	rootDir string
-	config  Config
-	logger  *slog.Logger
-	server  *http.Server
+	rootDir   string
+	config    Config
+	logger    *slog.Logger
+	server    *http.Server
+	multipart *multipart.Store
 }
 
 // New creates a new S3 Gateway.
 func New(rootDir string, cfg Config, logger *slog.Logger) *Gateway {
 	g := &Gateway{
-		rootDir: rootDir,
-		config:  cfg,
-		logger:  logger.With("gateway", "s3"),
+		rootDir:   rootDir,
+		config:    cfg,
+		logger:    logger.With("gateway", "s3"),
+		multipart: cfg.Multipart,
 	}
 
 	mux := http.NewServeMux()
@@ -160,6 +167,10 @@ func (g *Gateway) route(w http.ResponseWriter, r *http.Request) {
 func (g *Gateway) routeBucketOrObject(w http.ResponseWriter, r *http.Request, bucket, key string) {
 	if key == "" {
 		query := r.URL.Query()
+		if _, ok := query["uploads"]; ok && r.Method == http.MethodGet {
+			g.handleListMultipartUploads(w, r, bucket)
+			return
+		}
 		if _, ok := query["location"]; ok && r.Method == http.MethodGet {
 			g.handleGetBucketLocation(w, r, bucket)
 			return
@@ -193,6 +204,30 @@ func (g *Gateway) routeBucketOrObject(w http.ResponseWriter, r *http.Request, bu
 		default:
 			writeS3Error(w, http.StatusMethodNotAllowed, "MethodNotAllowed", "Method not allowed")
 		}
+		return
+	}
+
+	// Multipart operations are selected by query parameters on the object URL:
+	// ?uploads starts one, and ?uploadId=... addresses an existing one.
+	query := r.URL.Query()
+	_, initiating := query["uploads"]
+	uploadID := query.Get("uploadId")
+
+	switch {
+	case initiating && r.Method == http.MethodPost:
+		g.handleCreateMultipartUpload(w, r, bucket, key)
+		return
+	case uploadID != "" && r.Method == http.MethodPut:
+		g.handleUploadPart(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodPost:
+		g.handleCompleteMultipartUpload(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodDelete:
+		g.handleAbortMultipartUpload(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodGet:
+		g.handleListParts(w, r, bucket, key, uploadID)
 		return
 	}
 

internal/gateway/webdav/handlers.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/solkin/sebastian/internal/gateway"
 )
 
 // errUploadTooLarge is returned by the COPY copier when the configured upload
@@ -81,6 +83,9 @@ func (g *Gateway) handlePropfind(w http.ResponseWriter, r *http.Request) {
 			for {
 				children, rerr := f.ReadDir(512)
 				for _, child := range children {
+					if gateway.IsReservedPath(g.rootDir, filepath.Join(fullPath, child.Name())) {
+						continue
+					}
 					childRel := child.Name()
 					if relName != "" {
 						childRel = relName + "/" + childRel

solkin/birak

cmd/birakd/main.go
@@ -19,6 +19,7 @@ import (
 	s3gw "github.com/birak/birak/internal/gateway/s3"
 	sftpgw "github.com/birak/birak/internal/gateway/sftp"
 	webdavgw "github.com/birak/birak/internal/gateway/webdav"
+	"github.com/birak/birak/internal/multipart"
 	"github.com/birak/birak/internal/server"
 	"github.com/birak/birak/internal/store"
 	"github.com/birak/birak/internal/syncer"
@@ -184,7 +185,23 @@ func run(configPath string) error {
 
 	// S3 Gateway (if enabled).
 	var s3Gateway *s3gw.Gateway
+	var multipartStore *multipart.Store
 	if cfg.Gateways.S3.Enabled {
+		multipartStore, err = multipart.New(cfg.SyncDir, multipart.Limits{
+			MinPartBytes:       cfg.Multipart.MinPartBytes,
+			MaxPartBytes:       cfg.Multipart.MaxPartBytes,
+			MaxParts:           cfg.Multipart.MaxParts,
+			MaxObjectBytes:     cfg.MaxUploadBytes,
+			MaxActiveUploads:   cfg.Multipart.MaxActiveUploads,
+			MaxConcurrentParts: cfg.Multipart.MaxConcurrentPartUploads,
+			UploadTTL:          cfg.Multipart.UploadTTL,
+			CleanupInterval:    cfg.Multipart.CleanupInterval,
+			TempFileMaxAge:     cfg.Multipart.TempFileMaxAge,
+		}, logger)
+		if err != nil {
+			return fmt.Errorf("multipart store init: %w", err)
+		}
+
 		s3Gateway = s3gw.New(
 			cfg.SyncDir,
 			cfg.Ignore,
@@ -194,6 +211,7 @@ func run(configPath string) error {
 				SecretKey:      cfg.Gateways.S3.SecretKey,
 				Domain:         cfg.Gateways.S3.Domain,
 				MaxUploadBytes: cfg.MaxUploadBytes,
+				Multipart:      multipartStore,
 			},
 			logger,
 		)
@@ -204,6 +222,12 @@ func run(configPath string) error {
 				logger.Error("S3 gateway error", "error", err)
 				cancel()
 			}
+		}()
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			multipart.NewJanitor(multipartStore, logger).Run(ctx)
 		}()
 	}
 

internal/config/config.go
@@ -21,9 +21,35 @@ type Config struct {
 	Ignore     []string `yaml:"ignore"`
 	// MaxUploadBytes caps the size of a single uploaded object/file across all
 	// gateways (S3, WebDAV, HTTP UI, SFTP). 0 means unlimited.
-	MaxUploadBytes int64          `yaml:"max_upload_bytes"`
-	Sync           SyncConfig     `yaml:"sync"`
-	Gateways       GatewaysConfig `yaml:"gateways"`
+	MaxUploadBytes int64           `yaml:"max_upload_bytes"`
+	Multipart      MultipartConfig `yaml:"multipart"`
+	Sync           SyncConfig      `yaml:"sync"`
+	Gateways       GatewaysConfig  `yaml:"gateways"`
+}
+
+// MultipartConfig holds limits and retention settings for S3 multipart uploads.
+// Sizes are in bytes; durations use Go duration syntax ("24h", "30m").
+type MultipartConfig struct {
+	// MinPartBytes is the minimum size of every part but the last (S3: 5 MiB).
+	MinPartBytes int64 `yaml:"min_part_bytes"`
+	// MaxPartBytes is the maximum size of a single part (S3: 5 GiB).
+	MaxPartBytes int64 `yaml:"max_part_bytes"`
+	// MaxParts is the highest accepted part number (S3: 10000).
+	MaxParts int `yaml:"max_parts"`
+	// MaxActiveUploads caps simultaneously staged uploads; 0 means unlimited.
+	MaxActiveUploads int `yaml:"max_active_uploads"`
+	// MaxConcurrentPartUploads caps in-flight part uploads across all uploads;
+	// 0 means unlimited. Requests over the cap are rejected with SlowDown, which
+	// every S3 SDK retries with backoff.
+	MaxConcurrentPartUploads int `yaml:"max_concurrent_part_uploads"`
+	// UploadTTL is how long an untouched incomplete upload is kept before the
+	// janitor discards it.
+	UploadTTL time.Duration `yaml:"upload_ttl"`
+	// CleanupInterval is how often the janitor sweeps.
+	CleanupInterval time.Duration `yaml:"cleanup_interval"`
+	// TempFileMaxAge is how old an orphaned atomic-write scratch file must be
+	// before a running server removes it.
+	TempFileMaxAge time.Duration `yaml:"temp_file_max_age"`
 }
 
 // GatewaysConfig holds configuration for all gateways.
@@ -264,6 +290,44 @@ func applyEnv(c *Config) {
 	if v := os.Getenv("BIRAK_SFTP_HOST_KEY_PATH"); v != "" {
 		c.Gateways.SFTP.HostKeyPath = v
 	}
+
+	envInt64Map := map[string]*int64{
+		"BIRAK_MULTIPART_MIN_PART_BYTES": &c.Multipart.MinPartBytes,
+		"BIRAK_MULTIPART_MAX_PART_BYTES": &c.Multipart.MaxPartBytes,
+	}
+	for env, ptr := range envInt64Map {
+		if v := os.Getenv(env); v != "" {
+			if n, err := strconv.ParseInt(strings.TrimSpace(v), 10, 64); err == nil && n >= 0 {
+				*ptr = n
+			}
+		}
+	}
+
+	envIntMap := map[string]*int{
+		"BIRAK_MULTIPART_MAX_PARTS":                   &c.Multipart.MaxParts,
+		"BIRAK_MULTIPART_MAX_ACTIVE_UPLOADS":          &c.Multipart.MaxActiveUploads,
+		"BIRAK_MULTIPART_MAX_CONCURRENT_PART_UPLOADS": &c.Multipart.MaxConcurrentPartUploads,
+	}
+	for env, ptr := range envIntMap {
+		if v := os.Getenv(env); v != "" {
+			if n, err := strconv.Atoi(strings.TrimSpace(v)); err == nil && n >= 0 {
+				*ptr = n
+			}
+		}
+	}
+
+	envDurationMap := map[string]*time.Duration{
+		"BIRAK_MULTIPART_UPLOAD_TTL":        &c.Multipart.UploadTTL,
+		"BIRAK_MULTIPART_CLEANUP_INTERVAL":  &c.Multipart.CleanupInterval,
+		"BIRAK_MULTIPART_TEMP_FILE_MAX_AGE": &c.Multipart.TempFileMaxAge,
+	}
+	for env, ptr := range envDurationMap {
+		if v := os.Getenv(env); v != "" {
+			if d, err := time.ParseDuration(strings.TrimSpace(v)); err == nil && d > 0 {
+				*ptr = d
+			}
+		}
+	}
 }
 
 func parseBool(s string) bool {
@@ -312,6 +376,24 @@ func (c *Config) validate() error {
 	}
 	if c.Gateways.SFTP.Enabled && c.Gateways.SFTP.ListenAddr == "" {
 		return fmt.Errorf("gateways.sftp.listen_addr is required when SFTP gateway is enabled")
+	}
+	m := c.Multipart
+	if m.MinPartBytes < 0 || m.MaxPartBytes < 0 {
+		return fmt.Errorf("multipart part size limits must not be negative")
+	}
+	// A minimum above the maximum would reject every multi-part upload with a
+	// contradictory pair of errors, so refuse it at startup instead.
+	if m.MinPartBytes > 0 && m.MaxPartBytes > 0 && m.MinPartBytes > m.MaxPartBytes {
+		return fmt.Errorf("multipart.min_part_bytes must not exceed multipart.max_part_bytes")
+	}
+	if m.MaxParts < 0 {
+		return fmt.Errorf("multipart.max_parts must not be negative")
+	}
+	if m.MaxActiveUploads < 0 || m.MaxConcurrentPartUploads < 0 {
+		return fmt.Errorf("multipart upload count limits must not be negative")
+	}
+	if m.UploadTTL < 0 || m.CleanupInterval < 0 || m.TempFileMaxAge < 0 {
+		return fmt.Errorf("multipart durations must not be negative")
 	}
 	return nil
 }

internal/gateway/s3/s3.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/birak/birak/internal/gateway"
+	"github.com/birak/birak/internal/multipart"
 	"github.com/birak/birak/internal/watcher"
 )
 
@@ -22,6 +23,10 @@ type Config struct {
 	SecretKey      string `yaml:"secret_key"`
 	Domain         string `yaml:"domain"`
 	MaxUploadBytes int64  // max object size for PutObject; 0 = unlimited
+	// Multipart is the staging store backing the multipart upload API. When nil,
+	// multipart operations answer NotImplemented and the rest of the gateway is
+	// unaffected.
+	Multipart *multipart.Store
 }
 
 // Gateway implements the S3-compatible API.
@@ -31,6 +36,7 @@ type Gateway struct {
 	config         Config
 	logger         *slog.Logger
 	server         *http.Server
+	multipart      *multipart.Store
 }
 
 // New creates a new S3 Gateway.
@@ -40,6 +46,7 @@ func New(syncDir string, ignorePatterns []string, cfg Config, logger *slog.Logge
 		ignorePatterns: ignorePatterns,
 		config:         cfg,
 		logger:         logger.With("gateway", "s3"),
+		multipart:      cfg.Multipart,
 	}
 
 	mux := http.NewServeMux()
@@ -173,6 +180,10 @@ func (g *Gateway) routeBucketOrObject(w http.ResponseWriter, r *http.Request, bu
 	if key == "" {
 		// Check for sub-resource queries before bucket-level operations.
 		query := r.URL.Query()
+		if _, ok := query["uploads"]; ok && r.Method == http.MethodGet {
+			g.handleListMultipartUploads(w, r, bucket)
+			return
+		}
 		if _, ok := query["location"]; ok && r.Method == http.MethodGet {
 			g.handleGetBucketLocation(w, r, bucket)
 			return
@@ -213,6 +224,30 @@ func (g *Gateway) routeBucketOrObject(w http.ResponseWriter, r *http.Request, bu
 	// Check ignore patterns on key.
 	if watcher.ShouldIgnore(key, g.ignorePatterns) {
 		writeS3Error(w, http.StatusNotFound, "NoSuchKey", "The specified key does not exist.")
+		return
+	}
+
+	// Multipart operations are selected by query parameters on the object URL:
+	// ?uploads starts one, and ?uploadId=... addresses an existing one.
+	query := r.URL.Query()
+	_, initiating := query["uploads"]
+	uploadID := query.Get("uploadId")
+
+	switch {
+	case initiating && r.Method == http.MethodPost:
+		g.handleCreateMultipartUpload(w, r, bucket, key)
+		return
+	case uploadID != "" && r.Method == http.MethodPut:
+		g.handleUploadPart(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodPost:
+		g.handleCompleteMultipartUpload(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodDelete:
+		g.handleAbortMultipartUpload(w, r, bucket, key, uploadID)
+		return
+	case uploadID != "" && r.Method == http.MethodGet:
+		g.handleListParts(w, r, bucket, key, uploadID)
 		return
 	}
 

internal/gateway/webdav/handlers.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/birak/birak/internal/gateway"
 	"github.com/birak/birak/internal/watcher"
 )
 
@@ -83,6 +84,9 @@ func (g *Gateway) handlePropfind(w http.ResponseWriter, r *http.Request) {
 			for {
 				children, rerr := f.ReadDir(512)
 				for _, child := range children {
+					if gateway.IsReservedPath(g.syncDir, filepath.Join(fullPath, child.Name())) {
+						continue
+					}
 					childRel := child.Name()
 					if relName != "" {
 						childRel = relName + "/" + childRel