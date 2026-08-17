solkin/sebastian

Cover multipart uploads, limits, and staging isolation with tests

internal/config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestLoad_Defaults(t *testing.T) {
@@ -232,4 +233,113 @@ func writeYAML(t *testing.T, content string) string {
 		t.Fatalf("write yaml: %v", err)
 	}
 	return path
+}
+
+func TestLoad_MultipartDefaults(t *testing.T) {
+	path := writeYAML(t, `
+root_dir: /tmp/test
+gateways:
+  s3:
+    enabled: true
+`)
+	cfg, err := Load(path)
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	// Unset multipart fields stay zero; the store resolves them to the
+	// S3-compatible defaults so there is one source of truth.
+	if cfg.Multipart != (MultipartConfig{}) {
+		t.Fatalf("expected zero multipart config, got %+v", cfg.Multipart)
+	}
+}
+
+func TestLoad_MultipartConfig(t *testing.T) {
+	path := writeYAML(t, `
+root_dir: /tmp/test
+multipart:
+  min_part_bytes: 1048576
+  max_part_bytes: 104857600
+  max_parts: 500
+  max_active_uploads: 20
+  max_concurrent_part_uploads: 4
+  upload_ttl: 48h
+  cleanup_interval: 15m
+  temp_file_max_age: 6h
+gateways:
+  s3:
+    enabled: true
+`)
+	cfg, err := Load(path)
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	m := cfg.Multipart
+	if m.MinPartBytes != 1048576 || m.MaxPartBytes != 104857600 {
+		t.Fatalf("part size limits = %d/%d", m.MinPartBytes, m.MaxPartBytes)
+	}
+	if m.MaxParts != 500 || m.MaxActiveUploads != 20 || m.MaxConcurrentPartUploads != 4 {
+		t.Fatalf("count limits = %+v", m)
+	}
+	if m.UploadTTL != 48*time.Hour || m.CleanupInterval != 15*time.Minute || m.TempFileMaxAge != 6*time.Hour {
+		t.Fatalf("durations = %v/%v/%v", m.UploadTTL, m.CleanupInterval, m.TempFileMaxAge)
+	}
+}
+
+func TestLoad_MultipartEnvOverride(t *testing.T) {
+	path := writeYAML(t, `
+root_dir: /tmp/test
+multipart:
+  max_parts: 500
+  upload_ttl: 48h
+gateways:
+  s3:
+    enabled: true
+`)
+	t.Setenv("SEBASTIAN_MULTIPART_MAX_PARTS", "77")
+	t.Setenv("SEBASTIAN_MULTIPART_MIN_PART_BYTES", "2048")
+	t.Setenv("SEBASTIAN_MULTIPART_MAX_CONCURRENT_PART_UPLOADS", "8")
+	t.Setenv("SEBASTIAN_MULTIPART_UPLOAD_TTL", "90m")
+
+	cfg, err := Load(path)
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	if cfg.Multipart.MaxParts != 77 {
+		t.Fatalf("max_parts = %d, want 77", cfg.Multipart.MaxParts)
+	}
+	if cfg.Multipart.MinPartBytes != 2048 {
+		t.Fatalf("min_part_bytes = %d, want 2048", cfg.Multipart.MinPartBytes)
+	}
+	if cfg.Multipart.MaxConcurrentPartUploads != 8 {
+		t.Fatalf("max_concurrent_part_uploads = %d, want 8", cfg.Multipart.MaxConcurrentPartUploads)
+	}
+	if cfg.Multipart.UploadTTL != 90*time.Minute {
+		t.Fatalf("upload_ttl = %v, want 90m", cfg.Multipart.UploadTTL)
+	}
+}
+
+func TestLoad_MultipartInvalidValues(t *testing.T) {
+	cases := map[string]string{
+		"min above max": `
+multipart:
+  min_part_bytes: 100
+  max_part_bytes: 10
+`,
+		"negative max parts": `
+multipart:
+  max_parts: -1
+`,
+		"negative ttl": `
+multipart:
+  upload_ttl: -1h
+`,
+	}
+	for name, fragment := range cases {
+		t.Run(name, func(t *testing.T) {
+			path := writeYAML(t, "root_dir: /tmp/test\ngateways:\n  s3:\n    enabled: true\n"+fragment)
+			if _, err := Load(path); err == nil {
+				t.Fatal("expected an error")
+			}
+		})
+	}
 }

internal/gateway/gateway_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestSafePath_Root(t *testing.T) {
@@ -207,5 +208,78 @@ func TestResponseLogger_DefaultStatus(t *testing.T) {
 	}
 	if rl.Size != 4 {
 		t.Fatalf("expected size 4, got %d", rl.Size)
+	}
+}
+
+func TestSafePath_ReservedDir(t *testing.T) {
+	// The reserved state directory holds staged multipart parts; no protocol may
+	// resolve a path into it.
+	for _, p := range []string{
+		ReservedDirName,
+		"/" + ReservedDirName,
+		ReservedDirName + "/multipart",
+		"/" + ReservedDirName + "/multipart/abc/upload.json",
+		"./" + ReservedDirName,
+	} {
+		if _, _, err := SafePath("/data", p); err == nil {
+			t.Fatalf("SafePath(%q) succeeded, want an error", p)
+		}
+	}
+
+	// A name that merely starts with the reserved name is a normal path.
+	if _, _, err := SafePath("/data", ReservedDirName+"-backup"); err != nil {
+		t.Fatalf("SafePath(%q): %v", ReservedDirName+"-backup", err)
+	}
+}
+
+func TestIsReservedPath(t *testing.T) {
+	if !IsReservedPath("/data", "/data/"+ReservedDirName) {
+		t.Fatal("reserved dir not recognized")
+	}
+	if IsReservedPath("/data", "/data/photos") {
+		t.Fatal("normal dir reported as reserved")
+	}
+	// Only the top-level entry is reserved; the same name deeper down is not.
+	if IsReservedPath("/data", "/data/photos/"+ReservedDirName) {
+		t.Fatal("nested dir with the reserved name reported as reserved")
+	}
+}
+
+func TestSweepTempFiles_AgeThreshold(t *testing.T) {
+	root := t.TempDir()
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+	fresh := filepath.Join(root, ".seb-tmp-active")
+	stale := filepath.Join(root, ".seb-bak-orphan")
+	keep := filepath.Join(root, "regular.txt")
+	for _, p := range []string{fresh, stale, keep} {
+		if err := os.WriteFile(p, []byte("x"), 0o600); err != nil {
+			t.Fatalf("write %s: %v", p, err)
+		}
+	}
+	past := time.Now().Add(-2 * time.Hour)
+	if err := os.Chtimes(stale, past, past); err != nil {
+		t.Fatalf("chtimes: %v", err)
+	}
+
+	SweepTempFiles(root, time.Hour, logger)
+
+	if _, err := os.Stat(fresh); err != nil {
+		t.Fatalf("a scratch file younger than maxAge was removed: %v", err)
+	}
+	if _, err := os.Stat(stale); !os.IsNotExist(err) {
+		t.Fatalf("stale scratch file survived: %v", err)
+	}
+	if _, err := os.Stat(keep); err != nil {
+		t.Fatalf("regular file was removed: %v", err)
+	}
+
+	// maxAge <= 0 clears everything, which is what startup does.
+	SweepTempFiles(root, 0, logger)
+	if _, err := os.Stat(fresh); !os.IsNotExist(err) {
+		t.Fatal("unbounded sweep left a scratch file behind")
+	}
+	if _, err := os.Stat(keep); err != nil {
+		t.Fatalf("regular file was removed by the unbounded sweep: %v", err)
 	}
 }

internal/gateway/sftp/sftp_test.go
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	"golang.org/x/crypto/ssh"
+
+	"github.com/solkin/sebastian/internal/gateway"
 )
 
 // testGateway starts an SFTP gateway on a random port and returns its address.
@@ -1787,5 +1789,74 @@ func TestSubsystemMalformedPayload_NoPanic(t *testing.T) {
 	}
 	if pktType != sshFxpName {
 		t.Fatalf("expected NAME reply after malformed request, got %d", pktType)
+	}
+}
+
+func TestReaddirHidesReservedDir(t *testing.T) {
+	syncDir := t.TempDir()
+	if err := os.Mkdir(filepath.Join(syncDir, gateway.ReservedDirName), 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(syncDir, "a.txt"), []byte("a"), 0o644); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	addr := testGateway(t, syncDir, "user", "pass")
+	ch := sftpClient(t, addr, "user", "pass")
+	sftpInit(t, ch)
+
+	handle := sftpOpendir(t, ch, 1, "/")
+	names := sftpReaddir(t, ch, 2, handle)
+	for _, n := range names {
+		if n == gateway.ReservedDirName {
+			t.Fatalf("reserved dir listed: %v", names)
+		}
+	}
+	if len(names) != 1 || names[0] != "a.txt" {
+		t.Fatalf("listing = %v, want [a.txt]", names)
+	}
+}
+
+func TestOpendirReservedDirDenied(t *testing.T) {
+	syncDir := t.TempDir()
+	reserved := filepath.Join(syncDir, gateway.ReservedDirName)
+	if err := os.Mkdir(reserved, 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(reserved, "staged"), []byte("part"), 0o600); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	addr := testGateway(t, syncDir, "user", "pass")
+	ch := sftpClient(t, addr, "user", "pass")
+	sftpInit(t, ch)
+
+	// Both the directory itself and files inside it are unreachable.
+	var req []byte
+	req = marshalUint32(req, 1)
+	req = marshalString(req, "/"+gateway.ReservedDirName)
+	if err := writePacket(ch, sshFxpOpendir, req); err != nil {
+		t.Fatalf("write opendir: %v", err)
+	}
+	pktType, _, err := readPacket(ch)
+	if err != nil {
+		t.Fatalf("read opendir reply: %v", err)
+	}
+	if pktType != sshFxpStatus {
+		t.Fatalf("opendir on the reserved dir was allowed (packet type %d)", pktType)
+	}
+
+	req = nil
+	req = marshalUint32(req, 2)
+	req = marshalString(req, "/"+gateway.ReservedDirName+"/staged")
+	if err := writePacket(ch, sshFxpStat, req); err != nil {
+		t.Fatalf("write stat: %v", err)
+	}
+	pktType, _, err = readPacket(ch)
+	if err != nil {
+		t.Fatalf("read stat reply: %v", err)
+	}
+	if pktType != sshFxpStatus {
+		t.Fatalf("stat inside the reserved dir was allowed (packet type %d)", pktType)
 	}
 }

internal/gateway/webdav/webdav_test.go
@@ -11,6 +11,8 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/solkin/sebastian/internal/gateway"
 )
 
 // newTestGateway creates a Gateway with a temporary rootDir and httptest server.
@@ -1233,5 +1235,55 @@ func TestAuth_Unauthorized(t *testing.T) {
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusUnauthorized {
 		t.Fatalf("expected 401, got %d", resp.StatusCode)
+	}
+}
+
+func TestPropfind_HidesReservedDir(t *testing.T) {
+	g, ts := newTestGateway(t, "", "")
+	if err := os.Mkdir(filepath.Join(g.rootDir, gateway.ReservedDirName), 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(g.rootDir, "a.txt"), []byte("a"), 0o644); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	resp := doReq(t, "PROPFIND", ts.URL+"/", "", map[string]string{"Depth": "1"})
+	body := readBody(t, resp)
+	if resp.StatusCode != http.StatusMultiStatus {
+		t.Fatalf("status %d: %s", resp.StatusCode, body)
+	}
+	if strings.Contains(body, gateway.ReservedDirName) {
+		t.Fatalf("reserved dir appeared in PROPFIND output: %s", body)
+	}
+	if !strings.Contains(body, "a.txt") {
+		t.Fatalf("regular file missing from PROPFIND output: %s", body)
+	}
+}
+
+func TestAccess_ReservedDirIsDenied(t *testing.T) {
+	g, ts := newTestGateway(t, "", "")
+	reserved := filepath.Join(g.rootDir, gateway.ReservedDirName)
+	if err := os.Mkdir(reserved, 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(reserved, "staged"), []byte("part"), 0o600); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	cases := []struct{ method, path string }{
+		{"PROPFIND", "/" + gateway.ReservedDirName},
+		{http.MethodGet, "/" + gateway.ReservedDirName + "/staged"},
+		{http.MethodDelete, "/" + gateway.ReservedDirName + "/staged"},
+		{http.MethodPut, "/" + gateway.ReservedDirName + "/injected"},
+	}
+	for _, tc := range cases {
+		resp := doReq(t, tc.method, ts.URL+tc.path, "data", nil)
+		body := readBody(t, resp)
+		if resp.StatusCode < 400 {
+			t.Fatalf("%s %s was allowed: status %d, body %s", tc.method, tc.path, resp.StatusCode, body)
+		}
+	}
+	if _, err := os.Stat(filepath.Join(reserved, "injected")); !os.IsNotExist(err) {
+		t.Fatal("a write into the reserved dir succeeded")
 	}
 }

solkin/birak

internal/config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func writeYAML(t *testing.T, content string) string {
@@ -122,5 +123,100 @@ func TestParseBool(t *testing.T) {
 		if got := parseBool(tt.input); got != tt.want {
 			t.Errorf("parseBool(%q) = %v, want %v", tt.input, got, tt.want)
 		}
+	}
+}
+
+func TestLoad_MultipartDefaults(t *testing.T) {
+	cfg, err := Load("")
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	// Unset multipart fields stay zero; the store resolves them to the
+	// S3-compatible defaults so there is one source of truth.
+	if cfg.Multipart != (MultipartConfig{}) {
+		t.Fatalf("expected zero multipart config, got %+v", cfg.Multipart)
+	}
+}
+
+func TestLoad_MultipartConfig(t *testing.T) {
+	path := writeYAML(t, `
+multipart:
+  min_part_bytes: 1048576
+  max_part_bytes: 104857600
+  max_parts: 500
+  max_active_uploads: 20
+  max_concurrent_part_uploads: 4
+  upload_ttl: 48h
+  cleanup_interval: 15m
+  temp_file_max_age: 6h
+`)
+	cfg, err := Load(path)
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	m := cfg.Multipart
+	if m.MinPartBytes != 1048576 || m.MaxPartBytes != 104857600 {
+		t.Fatalf("part size limits = %d/%d", m.MinPartBytes, m.MaxPartBytes)
+	}
+	if m.MaxParts != 500 || m.MaxActiveUploads != 20 || m.MaxConcurrentPartUploads != 4 {
+		t.Fatalf("count limits = %+v", m)
+	}
+	if m.UploadTTL != 48*time.Hour || m.CleanupInterval != 15*time.Minute || m.TempFileMaxAge != 6*time.Hour {
+		t.Fatalf("durations = %v/%v/%v", m.UploadTTL, m.CleanupInterval, m.TempFileMaxAge)
+	}
+}
+
+func TestLoad_MultipartEnvOverride(t *testing.T) {
+	path := writeYAML(t, `
+multipart:
+  max_parts: 500
+  upload_ttl: 48h
+`)
+	t.Setenv("BIRAK_MULTIPART_MAX_PARTS", "77")
+	t.Setenv("BIRAK_MULTIPART_MIN_PART_BYTES", "2048")
+	t.Setenv("BIRAK_MULTIPART_MAX_CONCURRENT_PART_UPLOADS", "8")
+	t.Setenv("BIRAK_MULTIPART_UPLOAD_TTL", "90m")
+
+	cfg, err := Load(path)
+	if err != nil {
+		t.Fatalf("load: %v", err)
+	}
+	if cfg.Multipart.MaxParts != 77 {
+		t.Fatalf("max_parts = %d, want 77", cfg.Multipart.MaxParts)
+	}
+	if cfg.Multipart.MinPartBytes != 2048 {
+		t.Fatalf("min_part_bytes = %d, want 2048", cfg.Multipart.MinPartBytes)
+	}
+	if cfg.Multipart.MaxConcurrentPartUploads != 8 {
+		t.Fatalf("max_concurrent_part_uploads = %d, want 8", cfg.Multipart.MaxConcurrentPartUploads)
+	}
+	if cfg.Multipart.UploadTTL != 90*time.Minute {
+		t.Fatalf("upload_ttl = %v, want 90m", cfg.Multipart.UploadTTL)
+	}
+}
+
+func TestLoad_MultipartInvalidValues(t *testing.T) {
+	cases := map[string]string{
+		"min above max": `
+multipart:
+  min_part_bytes: 100
+  max_part_bytes: 10
+`,
+		"negative max parts": `
+multipart:
+  max_parts: -1
+`,
+		"negative ttl": `
+multipart:
+  upload_ttl: -1h
+`,
+	}
+	for name, fragment := range cases {
+		t.Run(name, func(t *testing.T) {
+			path := writeYAML(t, fragment)
+			if _, err := Load(path); err == nil {
+				t.Fatal("expected an error")
+			}
+		})
 	}
 }

internal/gateway/gateway_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestSafePath_Root(t *testing.T) {
@@ -243,5 +244,78 @@ func TestSweepTempFiles(t *testing.T) {
 	}
 	if _, err := os.Stat(bak); !os.IsNotExist(err) {
 		t.Errorf("stale .birak-bak file should be removed")
+	}
+}
+
+func TestSafePath_ReservedDir(t *testing.T) {
+	// The reserved state directory holds staged multipart parts; no protocol may
+	// resolve a path into it.
+	for _, p := range []string{
+		ReservedDirName,
+		"/" + ReservedDirName,
+		ReservedDirName + "/multipart",
+		"/" + ReservedDirName + "/multipart/abc/upload.json",
+		"./" + ReservedDirName,
+	} {
+		if _, _, err := SafePath("/data", p, nil); err == nil {
+			t.Fatalf("SafePath(%q) succeeded, want an error", p)
+		}
+	}
+
+	// A name that merely starts with the reserved name is a normal path.
+	if _, _, err := SafePath("/data", ReservedDirName+"-backup", nil); err != nil {
+		t.Fatalf("SafePath(%q): %v", ReservedDirName+"-backup", err)
+	}
+}
+
+func TestIsReservedPath(t *testing.T) {
+	if !IsReservedPath("/data", "/data/"+ReservedDirName) {
+		t.Fatal("reserved dir not recognized")
+	}
+	if IsReservedPath("/data", "/data/photos") {
+		t.Fatal("normal dir reported as reserved")
+	}
+	// Only the top-level entry is reserved; the same name deeper down is not.
+	if IsReservedPath("/data", "/data/photos/"+ReservedDirName) {
+		t.Fatal("nested dir with the reserved name reported as reserved")
+	}
+}
+
+func TestSweepTempFiles_AgeThreshold(t *testing.T) {
+	root := t.TempDir()
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+	fresh := filepath.Join(root, ".birak-tmp-active")
+	stale := filepath.Join(root, ".birak-bak-orphan")
+	keep := filepath.Join(root, "regular.txt")
+	for _, p := range []string{fresh, stale, keep} {
+		if err := os.WriteFile(p, []byte("x"), 0o600); err != nil {
+			t.Fatalf("write %s: %v", p, err)
+		}
+	}
+	past := time.Now().Add(-2 * time.Hour)
+	if err := os.Chtimes(stale, past, past); err != nil {
+		t.Fatalf("chtimes: %v", err)
+	}
+
+	SweepTempFiles(root, time.Hour, logger)
+
+	if _, err := os.Stat(fresh); err != nil {
+		t.Fatalf("a scratch file younger than maxAge was removed: %v", err)
+	}
+	if _, err := os.Stat(stale); !os.IsNotExist(err) {
+		t.Fatalf("stale scratch file survived: %v", err)
+	}
+	if _, err := os.Stat(keep); err != nil {
+		t.Fatalf("regular file was removed: %v", err)
+	}
+
+	// maxAge <= 0 clears everything, which is what startup does.
+	SweepTempFiles(root, 0, logger)
+	if _, err := os.Stat(fresh); !os.IsNotExist(err) {
+		t.Fatal("unbounded sweep left a scratch file behind")
+	}
+	if _, err := os.Stat(keep); err != nil {
+		t.Fatalf("regular file was removed by the unbounded sweep: %v", err)
 	}
 }

internal/gateway/sftp/sftp_test.go
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	"golang.org/x/crypto/ssh"
+
+	"github.com/birak/birak/internal/gateway"
 )
 
 // testGateway starts an SFTP gateway on a random port and returns its address.
@@ -1402,5 +1404,74 @@ func TestReaddirLargeDirectoryPaged(t *testing.T) {
 	}
 	if total != n {
 		t.Errorf("expected %d entries across paged readdir, got %d", n, total)
+	}
+}
+
+func TestReaddirHidesReservedDir(t *testing.T) {
+	syncDir := t.TempDir()
+	if err := os.Mkdir(filepath.Join(syncDir, gateway.ReservedDirName), 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(syncDir, "a.txt"), []byte("a"), 0o644); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	addr := testGateway(t, syncDir, nil, "user", "pass")
+	ch := sftpClient(t, addr, "user", "pass")
+	sftpInit(t, ch)
+
+	handle := sftpOpendir(t, ch, 1, "/")
+	names := sftpReaddir(t, ch, 2, handle)
+	for _, n := range names {
+		if n == gateway.ReservedDirName {
+			t.Fatalf("reserved dir listed: %v", names)
+		}
+	}
+	if len(names) != 1 || names[0] != "a.txt" {
+		t.Fatalf("listing = %v, want [a.txt]", names)
+	}
+}
+
+func TestOpendirReservedDirDenied(t *testing.T) {
+	syncDir := t.TempDir()
+	reserved := filepath.Join(syncDir, gateway.ReservedDirName)
+	if err := os.Mkdir(reserved, 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(reserved, "staged"), []byte("part"), 0o600); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	addr := testGateway(t, syncDir, nil, "user", "pass")
+	ch := sftpClient(t, addr, "user", "pass")
+	sftpInit(t, ch)
+
+	// Both the directory itself and files inside it are unreachable.
+	var req []byte
+	req = marshalUint32(req, 1)
+	req = marshalString(req, "/"+gateway.ReservedDirName)
+	if err := writePacket(ch, sshFxpOpendir, req); err != nil {
+		t.Fatalf("write opendir: %v", err)
+	}
+	pktType, _, err := readPacket(ch)
+	if err != nil {
+		t.Fatalf("read opendir reply: %v", err)
+	}
+	if pktType != sshFxpStatus {
+		t.Fatalf("opendir on the reserved dir was allowed (packet type %d)", pktType)
+	}
+
+	req = nil
+	req = marshalUint32(req, 2)
+	req = marshalString(req, "/"+gateway.ReservedDirName+"/staged")
+	if err := writePacket(ch, sshFxpStat, req); err != nil {
+		t.Fatalf("write stat: %v", err)
+	}
+	pktType, _, err = readPacket(ch)
+	if err != nil {
+		t.Fatalf("read stat reply: %v", err)
+	}
+	if pktType != sshFxpStatus {
+		t.Fatalf("stat inside the reserved dir was allowed (packet type %d)", pktType)
 	}
 }

internal/gateway/webdav/webdav_test.go
@@ -11,6 +11,8 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/birak/birak/internal/gateway"
 )
 
 // newTestGateway creates a Gateway with a temporary syncDir and httptest server.
@@ -939,5 +941,55 @@ func TestCopy_OverwritePreservesOnConflict(t *testing.T) {
 	}
 	if string(data) != "deep" {
 		t.Fatalf("expected 'deep', got %q", data)
+	}
+}
+
+func TestPropfind_HidesReservedDir(t *testing.T) {
+	g, ts := newTestGateway(t, nil, "", "")
+	if err := os.Mkdir(filepath.Join(g.syncDir, gateway.ReservedDirName), 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(g.syncDir, "a.txt"), []byte("a"), 0o644); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	resp := doReq(t, "PROPFIND", ts.URL+"/", "", map[string]string{"Depth": "1"})
+	body := readBody(t, resp)
+	if resp.StatusCode != http.StatusMultiStatus {
+		t.Fatalf("status %d: %s", resp.StatusCode, body)
+	}
+	if strings.Contains(body, gateway.ReservedDirName) {
+		t.Fatalf("reserved dir appeared in PROPFIND output: %s", body)
+	}
+	if !strings.Contains(body, "a.txt") {
+		t.Fatalf("regular file missing from PROPFIND output: %s", body)
+	}
+}
+
+func TestAccess_ReservedDirIsDenied(t *testing.T) {
+	g, ts := newTestGateway(t, nil, "", "")
+	reserved := filepath.Join(g.syncDir, gateway.ReservedDirName)
+	if err := os.Mkdir(reserved, 0o700); err != nil {
+		t.Fatalf("create reserved dir: %v", err)
+	}
+	if err := os.WriteFile(filepath.Join(reserved, "staged"), []byte("part"), 0o600); err != nil {
+		t.Fatalf("write file: %v", err)
+	}
+
+	cases := []struct{ method, path string }{
+		{"PROPFIND", "/" + gateway.ReservedDirName},
+		{http.MethodGet, "/" + gateway.ReservedDirName + "/staged"},
+		{http.MethodDelete, "/" + gateway.ReservedDirName + "/staged"},
+		{http.MethodPut, "/" + gateway.ReservedDirName + "/injected"},
+	}
+	for _, tc := range cases {
+		resp := doReq(t, tc.method, ts.URL+tc.path, "data", nil)
+		body := readBody(t, resp)
+		if resp.StatusCode < 400 {
+			t.Fatalf("%s %s was allowed: status %d, body %s", tc.method, tc.path, resp.StatusCode, body)
+		}
+	}
+	if _, err := os.Stat(filepath.Join(reserved, "injected")); !os.IsNotExist(err) {
+		t.Fatal("a write into the reserved dir succeeded")
 	}
 }